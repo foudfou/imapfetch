5d42d2a4fb1c02b02170986d6a4cb4a8cb9ddd6f
foudfou <foudil.newbie+git@gmail.com>
2016-01-04 20:05:02 +0100

    * Fetch UNDELETED messages instead of NEW ones. * Fetch ALL messages at startup. * Remove unused logs.

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ var fs = require('fs'),
 var Imap = require('imap');
 
 
-function die(retcode, msg) {
+function die(retCode, msg) {
   console.error(msg);
-  process.exit(retcode);
+  process.exit(retCode);
 }
 
 function openInbox(cb) {
-  var readonly = true;
-  var box = imap.openBox('INBOX', readonly, cb);
+  var readonly = false;
+  return imap.openBox('INBOX', readonly, cb);
 }
 
 
@@ -30,50 +30,45 @@ try {
 var imap = new Imap(config.imap);
 
 imap.once('ready', function() {
-  // we need to open INBOX once to trigger `mail'.
-  openInbox(function(err, box) {
+  var inbox = openInbox(function(err, box) {
     if (err) throw err;
-  });
-});
 
-imap.on('mail', function(numNewMsgs) {
-  // console.log("--mail-- "+numNewMsgs);
-  openInbox(function(err, box) {
-    if (err) throw err;
-    imap.search(['NEW'], function(err, results) {
-      if (err) throw err;
+    fetchMessages(['ALL']);
 
-      var f = imap.fetch(results, { bodies: '' });
-
-      f.on('message', function(msg, seqno) {
-        console.log('Message #%d', seqno);
-        var prefix = '(#' + seqno + ') ';
-        msg.on('body', function(stream, info) {
-          console.log(prefix + 'Body');
-          var mda = child.spawn(config.mda.cmd, config.mda.args,
-                                {stdio: ['pipe', 1, 2]});
-          stream.pipe(mda.stdin);
-        });
-        msg.once('attributes', function(attrs) {
-          console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
-        });
-        msg.once('end', function() {
-          console.log(prefix + 'Finished');
-        });
-      });
-      f.once('error', function(err) {
-        console.log('Fetch error: ' + err);
-      });
-      f.once('end', function() {
-        console.log('Done fetching all messages!');
-      });
-
+    imap.on('mail', function(numNewMsgs) {
+      console.info("New mail: "+numNewMsgs);
+      fetchMessages(['UNDELETED']);  // UNSEEN, NEW
     });
   });
 });
 
-imap.once('error', function(err) {
-  console.log(err);
+function fetchMessages(criteria) {
+  imap.search(criteria, function(err, uids) {
+    if (err) throw err;
+    if (!uids.length) return;
+
+    var f = imap.fetch(uids, { bodies: '' });
+    f.on('message', function(msg, seqno) {
+      console.info('Downloaded message #%d', seqno);
+      msg.on('body', function(stream, info) {
+        var mda = child.spawn(config.mda.cmd, config.mda.args,
+                              {stdio: ['pipe', 1, 2]});
+        stream.pipe(mda.stdin);
+      });
+    });
+    f.once('error', function(err) {
+      console.error('Fetch error: ' + err);
+    });
+    f.once('end', function() {
+      imap.addFlags(uids, '\\Deleted', function(err) {
+        if (err) console.error("addFlags failed: " + err);
+      });
+    });
+  });
+};
+
+imap.on('error', function(err) {
+  console.error("Connection error: " + err);
 });
 
 imap.once('end', function() {
@@ -81,9 +76,21 @@ imap.once('end', function() {
 });
 
 
-process.on('SIGINT', function() {
-  console.log("Caught interrupt signal");
+function quit() {
+  imap.expunge(function(err) {  // uids can be supplied as fist arg
+    if (err) console.error("Expunge failed: " + err);
+  });
   imap.end();
+}
+
+process.on('SIGINT', function() {
+  console.info("Caught interrupt signal");
+  quit();
+});
+
+process.on('SIGTERM', function() {
+  console.info("Caught termination signal");
+  quit();
 });
 
 imap.connect();
